8dff299ce3b925086de59cafdd9907d925a9682e
Marcosvpj <marcos@marcosvpj.com.br>
2018-02-13 23:47:29 -0200

    Lesson 4: Predict author problem with decision tree

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -25,7 +25,16 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+from sklearn.tree import DecisionTreeClassifier
 
+clf = DecisionTreeClassifier(min_samples_split=40)
+clf.fit(features_train, labels_train)
+
+clf.predict(features_test)
+
+score = clf.score(features_test, labels_test)
+
+print(score)
 #########################################################
 
 
